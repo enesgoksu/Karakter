bcfea77d015b4ffad706a03acd7748af43977433
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-04-05 15:27:57 +0300

    program.cs değiştirildi

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Karakter.Karakter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir Kelime ya da cümle yazınız: ");
+            string yazı = Console.ReadLine();
+            Console.Write("İndex numarası giriniz: ");
+            int index = int.Parse(Console.ReadLine());
+
+            yazı = yazı.Remove(index,1);
+            Console.WriteLine(yazı);
+        }
+    }
+}
